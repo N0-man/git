b2ed87cc7c38ad7584548c227d44afa5b14b8599
_n0man <nmemon@thoughtworks.com>
2019-05-17 05:12:54 +0530

    5. commit on feature1 again

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # git
 Playing with Git
-I am on git commit 2.
-I am on git commit master
+I am on git commit 5.
--- hello.hs
+++ hello.hs
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 main = putStrLn "Hello, best world world! from master"
+=======
+main = putStrLn "Hello, best world world! In Feature 1 and more comment"
+>>>>>>> 88aeb3b... 5. commit on feature1 again
