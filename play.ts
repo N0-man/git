50a7e1efec3a7633bab59e9af2bbc3e6818848d1
n0man <nouman.memon@icloud.com>
2023-04-05 09:32:22 +0800

    push with signed commit

--- play.ts
+++ play.ts
@@ -1,3 +1,6 @@
 console.log('hello typescript')
 console.log('adding another commit');
 console.log('yet adding another commit');
+const whatIsYourAge = () => {
+    console.log('Why should I tell you la?')
+}
